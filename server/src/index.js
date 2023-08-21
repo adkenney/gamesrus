f08ca0bec9e6bb00f462c9378311be2d78a06ed0
adkenney <arkellk@yahoo.com>
2023-08-21 13:10:19 -0400

    change server folder structure

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = require('./router');
 const morgan = require('morgan');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
@@ -6,5 +7,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(morgan('tiny'));
+app.use(router);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
