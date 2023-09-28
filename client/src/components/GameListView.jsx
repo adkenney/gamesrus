8bdab3ccf9ea5e0d9e5d6b9c6858ec6fc632d5ac
adkenney <arkellk@yahoo.com>
2023-09-27 20:48:28 -0400

    refactor and style GameListView component

--- client/src/components/GameListView.jsx
+++ client/src/components/GameListView.jsx
@@ -1,66 +1,35 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useMutation, useQueryClient } from 'react-query';
 import { useGetGames } from '../api/useGetGames';
-import UpdateGameForm from './UpdateGameForm';
+import Card from './Card';
 
 export default function GameListView() {
   const games = useGetGames();
-  const [selectedGame, setSelectedGame] = useState(null);
-  const queryClient = useQueryClient();
-  const deleteGame = useMutation({
-    mutationFn: async id => {
-      let deleteCode = prompt('Enter code to delete');
-      if (deleteCode === import.meta.env.VITE_SECRET_CODE) {
-        await fetch(`${import.meta.env.VITE_BASE_URL}/api/games/${id}`, {
-          method: 'DELETE',
-        });
-      } else {
-        alert("Sorry, you don't have permission to delete");
-      }
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['game'] });
-    },
-  });
 
-  const handleEdit = game => {
-    setSelectedGame(game);
-  };
-
-  return games ? (
-    <section>
-      {games &&
-        games.map(game => {
+  if (games) {
+    return (
+      <section className="game-grid">
+        {games.map(game => {
           return (
-            <div key={game._id}>
+            <Card key={game._id}>
               <h2>
                 <Link to={`/games/${game._id}`}>{game.title}</Link>
               </h2>
-              <p>{game.summary}</p>
-              <p>Release date: {game.release_date}</p>
-              <ul>
+              <p>Release date: {game.release_date.slice(0, 4)}</p>
+              <ul className="card-list">
                 {game.genre.map(genre => {
                   return <li key={genre._id}>{genre.name}</li>;
                 })}
               </ul>
-              <ul>
+              <ul className="card-list">
                 {game.platform.map(platform => {
                   return <li key={platform._id}>{platform.name}</li>;
                 })}
               </ul>
-              <p>{game.publisher.name}</p>
               <p>{game.price === 0 ? 'Free' : `$${game.price}`}</p>
-              <button onClick={() => deleteGame.mutate(game._id)}>
-                Delete
-              </button>
-              <button onClick={() => handleEdit(game)}>Edit</button>
-              {selectedGame && selectedGame._id === game._id && (
-                <UpdateGameForm game={selectedGame} />
-              )}
-            </div>
+            </Card>
           );
         })}
-    </section>
-  ) : null;
+      </section>
+    );
+  }
 }
